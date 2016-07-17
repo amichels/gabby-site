f1ba36340e3ea1eb806cb507fdec4c32ed49a572
amichels <andy.j.michels@gmail.com>
2016-07-17 15:22:06 -0500

    set up nunjucks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		nunjucksRender = require('gulp-nunjucks-render');
 
 gulp.task('styles', function() {
-    gulp.src('ui/sass/**/*.scss')
+    gulp.src('app/ui/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./ui/css/'));
+        .pipe(gulp.dest('./app/ui/css/'));
+});
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('app'))
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('app/ui/sass/**/*.scss',['styles']);
+    gulp.watch('app/templates/**/*.html',['nunjucks']);
 });
