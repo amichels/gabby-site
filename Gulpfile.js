b4936a00523c8bed4004639cf5c16145316d9abf
Andy Michels <amichels@whitinghouse.com>
2018-01-29 22:56:14 -0600

    backup of gabby site

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,28 +1,76 @@
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    path = require('path'),
 		autoprefixer = require('gulp-autoprefixer'),
 		nunjucksRender = require('gulp-nunjucks-render');
+    livereload = require('gulp-livereload'),
+    fs = require("fs"),
+    browserify = require("browserify"),
+    babelify = require("babelify"),
+    uglifyify = require("uglifyify");
 
-gulp.task('styles', function() {
-    gulp.src('app/ui/sass/**/*.scss')
+function npmModule(url, file, done) {
+  // check if the path was already found and cached
+  if(aliases[url]) {
+    return done({ file:aliases[url] });
+  }
+
+  // look for modules installed through npm
+  try {
+    var newPath = path.relative('./css', require.resolve(url));
+    aliases[url] = newPath; // cache this request
+    return done({ file:newPath });
+  } catch(e) {
+    // if your module could not be found, just return the original url
+    aliases[url] = url;
+    return done({ file:url });
+  }
+}
+
+// CSS
+gulp.task('css', function() {
+    gulp.src('./app/ui/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cssnano())
         .pipe(gulp.dest('./app/ui/css/'));
 });
 
-gulp.task('nunjucks', function() {
+// HTML
+gulp.task('html', function() {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  return gulp.src('./app/pages/**/*.+(html|nunjucks)')
   // Renders template with nunjucks
   .pipe(nunjucksRender({
-      path: ['app/templates']
+      path: ['./app/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('app'))
+  .pipe(gulp.dest('./'))
+});
+
+// JS
+gulp.task('js', function() {
+  return browserify("./app/ui/src-js/app.js")
+    .transform("babelify", {presets: ["es2015"]})
+    .transform("uglifyify")
+    .bundle()
+    .pipe(fs.createWriteStream("./app/ui/js/app.min.js"));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./app/ui/sass/**/*.scss',['css']);
+  gulp.watch('./app/templates/**/*.html',['html']);
+  gulp.watch(['./app/ui/src-js/**/*.js'],['js'])
+
+  // Create LiveReload server
+  livereload.listen();
+
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('app/ui/sass/**/*.scss',['styles']);
-    gulp.watch('app/templates/**/*.html',['nunjucks']);
+    gulp.start('css','html','js');
 });
